43e22595a784294587081ac0e7a43867eda8d5bc
luiz_laurenti@hotmail.com <luiz_laurenti@hotmail.com>
2020-07-31 00:41:49 -0300

    Initial category form implemented

--- src/pages/register/Category/index.js
+++ src/pages/register/Category/index.js
@@ -1,23 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CategoryRegistration() {
 
+    const initialValues = {
+        name: '',
+        description: '',
+        color: ''
+    }
+
+    const [categories, setCategories] = useState([]);
+    const [values, setValues] = useState(initialValues);
+
+    //TODO: undestand this '[]' behaviour
+    function setValue(key, value) {
+        setValues({
+            ...values,
+            [key]: value,
+        })
+    }
+
+    function handleChange(event) {
+        const { getAttribute, value } = event.target;
+ 
+        setValue(
+            getAttribute('name'),
+            value
+        );
+    }
+
+
     return (
         <PageDefault >
-            <h1>Category registration</h1>
+            <h1>Category registration: {values.name}</h1>
 
-            <form>
-                <label>
-                    Category name:
-                    <input type="text" />
-                </label>
+            <form onSubmit={(event) => {
+                event.preventDefault();
+                setCategories([
+                    ...categories,
+                    values
+                ]);
+
+                setValues({ initialValues })
+            }}>
+                <div>
+                    <label>
+                        Category name:
+                        <input
+                            type="text"
+                            name="name"
+                            value={values.name}
+                            onChange={handleChange}
+                        />
+                    </label>
+
+                    <label>
+                        Description:
+                        <textarea
+                            name="description"
+                            value={values.description}
+                            onChange={handleChange}
+                        />
+                    </label>
+
+                    <label>
+
+                        Color:
+                    <input
+                            name="color"
+                            type="color"
+                            value={values.color}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div>
 
                 <button>
                     Register
                 </button>
             </form>
+
+            <ul>
+                {categories.map((category, i) => {
+                    return (
+                        <li key={`${category}${i}`}>
+                            {category.name}
+                        </li>
+                    );
+                })}
+            </ul>
 
             <Link to="/">
                 Go to home
